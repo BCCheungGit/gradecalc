3413540e5945bd7630242836a4ec7f616e81787e
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-05-10 12:09:04 +0800

    added letter grade functionality

--- app.py
+++ app.py
@@ -10,14 +10,33 @@ def home():
     if request.method == 'POST': 
         grades = request.form.getlist("grades")
         weights = request.form.getlist("weights")
-        print(grades)
-        print(weights)
+        #print(grades)
+        #print(weights)
         weightsum = 0
         average = 0
         for i in range(0, len(grades)):
             weightsum += int(weights[i])
             average += int(grades[i]) * float((int(weights[i])/100))
         finalgrade = f'{(average / weightsum) * 100:.2f}'
+        lettergrades = ['A', 'A-', 'B', 'B-', 'C', 'C-', 'D', 'F']
+        letter = 'F'
 
-        return render_template('index.html', grade=finalgrade)
+        if float(finalgrade) >=95:
+            letter = lettergrades[0]
+        elif float(finalgrade) >= 90:
+            letter = lettergrades[1]
+        elif float(finalgrade) >= 85:
+            letter = lettergrades[2]
+        elif float(finalgrade) >= 80:
+            letter = lettergrades[3]
+        elif float(finalgrade) >= 75:
+            letter = lettergrades[4]
+        elif float(finalgrade) >= 70:
+            letter = lettergrades[5]
+        elif float(finalgrade) >= 65: 
+            letter = lettergrades[6]
+        else:
+            letter = lettergrades[7]
+
+        return render_template('index.html', grade=finalgrade, lettergrade=letter)
     return render_template('home.html')
